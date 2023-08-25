4b69d42c0621b029b2b56e8aaf01f1083076ed07
ShujaShah <shuja.ashraf11@gmail.com>
2023-08-25 11:19:01 +0530

    added structured errors

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,7 +1,19 @@
 import { ErrorRequestHandler } from 'express';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  console.log('Something went wrong💥', err);
+  if (err instanceof RequestValidationError) {
+    const formattedErrors = err.errors.map((error) => {
+      if (error.type === 'field') {
+        return { message: error.msg, field: error.path };
+      }
+    });
+    return res.status(400).send({ errors: formattedErrors });
+  }
+  if (err instanceof DatabaseConnectionError) {
+    console.log('handling this error as db connection error');
+  }
   res.status(400).send({
     message: 'Something went wrong💥',
   });

62f5fe54a71464ecac011c1c406de6fe1b3f3672
ShujaShah <shuja.ashraf11@gmail.com>
2023-08-25 12:27:11 +0530

    added the abstract class

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,10 +1,10 @@
-import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
 
-export class DatabaseConnectionError extends Error {
+export class DatabaseConnectionError extends CustomError {
   statusCode = 500;
   reason = 'Error Connecting to Database...';
   constructor() {
-    super();
+    super('Error connecting to Database...');
 
     // To extend a build in class
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,13 +1,9 @@
 import { ErrorRequestHandler } from 'express';
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { CustomError } from '../errors/custom-error';
 
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  if (err instanceof RequestValidationError) {
+  if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
-  }
-  if (err instanceof DatabaseConnectionError) {
-    res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
   res.status(400).send({
     errors: [

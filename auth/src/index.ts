9cd1ed38c4f14af86f19687f5946391dcdff7a23
ShujaShah <shuja.ashraf11@gmail.com>
2023-08-26 11:08:20 +0530

    added the mongodb connection

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
+
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signupRouter } from './routes/signup';
@@ -21,6 +23,16 @@ app.use(errorHandler);
 //   throw new NotFoundError();
 // });
 
-app.listen(3000, () => {
-  console.log('Listening on Port 3000!');
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to the Database...😄');
+  } catch (err) {
+    console.error(err);
+  }
+  app.listen(3000, () => {
+    console.log('Listening on Port 3000!');
+  });
+};
+
+start();

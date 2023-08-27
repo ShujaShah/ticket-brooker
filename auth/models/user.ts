f3a170a794c94f2fc6a2107c5619f921e5c96965
ShujaShah <shuja.ashraf11@gmail.com>
2023-08-27 12:05:30 +0530

    user model completion

--- auth/models/user.ts
+++ auth/models/user.ts
@@ -1,4 +1,21 @@
-import mongoose from 'mongoose';
+import mongoose, { mongo } from 'mongoose';
+
+// Describing the properties required to create a new user
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// Describing the properties that user model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+//Describing the properties that a user documet has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -11,5 +28,10 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
 export { User };

571766be18d828d140f4214624378579d29c3920
ShujaShah <shuja.ashraf11@gmail.com>
2023-08-29 11:45:03 +0530

    implemented signin

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,52 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { User } from '../models/user';
+import { validateRequest } from '../middlewares/validate-request';
+import { BadRequestError } from '../errors/bad-request-error';
+import { Password } from '../services/password';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('hi there!');
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().notEmpty().withMessage('Password is not correct'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    //check to see if the user exists or not
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError('Email does not exist');
+    }
+    const passwordsMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+    if (!passwordsMatch) {
+      throw new BadRequestError('Invalid Credentials');
+    }
+    //Generate a JWT
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY! // the exclamation mark tell typescript not to worry as we have already defined it..
+    );
+
+    //Store the jwt on session object
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(201).send(existingUser);
+  }
+);
 
 export { router as signinRouter };

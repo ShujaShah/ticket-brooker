561a1bcd5a3bc926f85f5b5f02e02d636f12cb45
ShujaShah <shuja.ashraf11@gmail.com>
2023-09-07 12:03:21 +0530

    adding router protector to the client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,10 @@
-const Landing = () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+  axios.get('/api/users/currentuser').catch((err) => {
+    console.log(err.message);
+  });
+
   return <h1>Landing Page</h1>;
 };
-
-export default Landing;
